b42f7133abf13a03c8e15344abeaac86297b17ff
Pip <pipcet@gmail.com>
2016-04-25 21:20:20 +0000

    fix struct stat layout test

--- tests/110-stat.c
+++ tests/110-stat.c
@@ -4,9 +4,24 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define P(str) printf(#str " %d %d", offsetof(struct stat, str), sizeof((struct stat *)0->str));
+#define offsetof(haystack, needle) ((unsigned)(&((haystack *)0)->needle))
 
-int main(void)
+#define P(str) printf(#str " %d %d %016llx\n", offsetof(struct stat, str), sizeof(((struct stat *)0)->str), (long long)buf.str);
+
+int main(int argc, char **argv)
 {
+  struct stat buf;
+  stat(argv[1], &buf);
   P(st_dev);
+  P(st_ino);
+  P(st_nlink);
+  P(st_mode);
+  P(st_uid);
+  P(st_gid);
+  P(st_rdev);
+  P(st_size);
+  P(st_blksize);
+  P(st_blocks);
+
+  return 0;
 }
