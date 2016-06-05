af08fea8839fa8278fe3d87513469ee3fc82d71d
Pip <pipcet@gmail.com>
2016-06-05 14:11:19 +0000

    jsexport WIP

--- include/jsexport.h
+++ include/jsexport.h
@@ -64,8 +64,12 @@ T from_double(double d)
 {
   T ret = T(d);
 
-  if (double(ret) != d)
-    throw "round-trip conversion failed";
+  if (double(ret) != d) 
+    {
+      printf("round-trip conversion failed; %f != %f\n",
+             double(ret), d);
+      throw "round-trip conversion failed";
+    }
 
   return ret;
 }
@@ -178,27 +182,34 @@ Lift<R, Ts...> lift(R f(Ts...))
 }
 
 asm(".include \"import-macros.s\"");
-asm(".import3 thinthin,queue_peek,__thinthin_queue_peek");
-asm(".import3 thinthin,queue_pop,__thinthin_queue_pop");
 asm(".import3 thinthin,export,__thinthin_export");
-
-extern "C"
-__attribute__((stackcall))
-int
-__thinthin_queue_peek();
-
-extern "C"
-__attribute__((stackcall))
-int
-__thinthin_queue_pop(void (**)(long, long, void *, double *),
-                     void *,
-                     void *);
+asm(".import3 thinthin,export_type,__thinthin_export_type");
+asm(".import3 thinthin,export_field,__thinthin_export_field");
+asm(".import3 thinthin,export_var,__thinthin_export_var");
 
 extern "C"
 __attribute__((stackcall))
 int
 __thinthin_export(const char *name, void *f, const char *type, void *apply,
                   const char *demangled_type, unsigned nargs);
+
+extern "C"
+__attribute__((stackcall))
+int
+__thinthin_export_type(const char *jsname, const char *typeidstr);
+
+extern "C"
+__attribute__((stackcall))
+int
+__thinthin_export_field(const char *jsname, const char *fieldname,
+                        const char *recordtype, const char *fieldtype,
+                        const char *getter, const char *setter);
+
+extern "C"
+__attribute__((stackcall))
+int
+__thinthin_export_var(const char *jsname, const char *type_jsname,
+                      unsigned long address);
 
 #if 0
 int
@@ -226,32 +237,135 @@ class AsmJSExport {
   public:
   AsmJSExport (const char *name,
                F f,
+               const char *classtype,
+               const char *rettype,
                const char *type)
     : name(name), f(f), type(type)
   {
-    printf("constructing AsmJSExport\n");
+    printf("constructing AsmJSExport %s/%s/%s\n",
+           classtype, rettype, type);
     auto L(lift(f));
     __thinthin_export(name, (void *)f, type, (void *)L.apply_doubles,
                       demangle(type), count_double_args<decltype(f)>::count());
-
-    int size;
-    while ((size = __thinthin_queue_peek()) >= 0) {
-      double stack[1+size];
-      void (*apply)(long, long, void *, double *);
-      void *fun;
-
-      __thinthin_queue_pop(&apply, &fun, stack+1);
-
-      void (*f)() = (void (*)())fun;
-      printf("queue pop returned %p %p=%p [%f]\n", apply, fun, f,
-             stack[1]);
-
-      apply(0, 0, fun, stack);
-
-      printf("rv %f\n", stack[0]);
-    }
   }
 };
 
-#define asmjs_register(name, f, f2, type)                           \
-  AsmJSExport<decltype(f2)> jsexport_##f(name, (decltype(f2))f2, type);
+template<class T, class F, unsigned long offset, unsigned long bytesize>
+class AsmJSExportField {
+public:
+  static F *getter(T* p)
+  {
+    char *p2 = (char *)p;
+
+    p2 += offset;
+
+    return (F *)p2;
+  }
+
+  static void setter(T* p, F *v)
+  {
+    char *p2 = (char *)p;
+
+    p2 += offset;
+
+    memcpy(p2, v, bytesize);
+  }
+
+  AsmJSExportField (const char *jsname, const char *fieldname,
+                    const char *recordtype, const char *fieldtype)
+  {
+    char *getter_name;
+    char *setter_name;
+
+    asprintf(&getter_name, "%s_%s__getter", jsname, fieldname);
+    asprintf(&setter_name, "%s_%s__setter", jsname, fieldname);
+
+    AsmJSExport<decltype(&getter)> jsexport_getter(getter_name, getter, recordtype, fieldtype, typeid(getter).name());
+    AsmJSExport<decltype(&setter)> jsexport_setter(setter_name, setter, recordtype, fieldtype, typeid(setter).name());
+
+    __thinthin_export_field(jsname, fieldname,
+                            recordtype, fieldtype,
+                            getter_name, setter_name);
+
+    free(getter_name);
+    free(setter_name);
+  }
+};
+
+template<class T,class F, unsigned long offset, unsigned long bytesize>
+requires(std::is_integral<F>::value || std::is_pointer<F>::value)
+class AsmJSExportField<T,F,offset,bytesize> {
+public:
+  static F getter(T* p)
+  {
+    char *p2 = (char *)p;
+
+    p2 += offset;
+
+    return *(F *)p2;
+  }
+
+  static void setter(T* p, F v)
+  {
+    char *p2 = (char *)p;
+
+    p2 += offset;
+
+    memcpy(p2, &v, bytesize);
+  }
+
+  AsmJSExportField (const char *jsname, const char *fieldname,
+                    const char *recordtype, const char *fieldtype)
+  {
+    char *getter_name;
+    char *setter_name;
+
+    asprintf(&getter_name, "%s_%s__getter", jsname, fieldname);
+    asprintf(&setter_name, "%s_%s__setter", jsname, fieldname);
+
+    AsmJSExport<decltype(&getter)> jsexport_getter(getter_name, getter, recordtype, fieldtype, typeid(getter).name());
+    AsmJSExport<decltype(&setter)> jsexport_setter(setter_name, setter, recordtype, fieldtype, typeid(setter).name());
+
+    __thinthin_export_field(jsname, fieldname,
+                            recordtype, fieldtype,
+                            getter_name, setter_name);
+
+    free(getter_name);
+    free(setter_name);
+  }
+};
+
+class AsmJSExportType {
+public:
+  AsmJSExportType (const char *jsname, const char *type)
+  {
+    __thinthin_export_type (jsname, type);
+  }
+};
+
+class AsmJSExportVar {
+public:
+  AsmJSExportVar (const char *jsname, const char *type_jsname, unsigned long address)
+  {
+    __thinthin_export_var (jsname, type_jsname, address);
+  }
+};
+
+/* Register a function or method. JSNAME is the JavaScript identifier
+   to be used; SUFFIX is a unique symbol suffix for this function;
+   FUNCTION is a function pointer; CLASSTYPE is the typeid string for
+   either void or a pointer to the class of which this function is a
+   method; RETTYPE is the typeid string for the function's return
+   type; TYPE is the typeid string for the entire function pointer. */
+
+#define asmjs_register_function(jsname, suffix, function, classtype, rettype, type)           \
+  AsmJSExport<decltype(function)> jsexport_##suffix(jsname, (decltype(function))function, classtype, rettype, type);
+
+#define asmjs_register_record_type(jsname, classtype) \
+  AsmJSExportType jsexport_##__LINE__(jsname, typeid(classtype *).name());
+
+#define asmjs_register_field(jsname, classtype, fieldtype, fieldname, bitpos, bytesize) \
+  AsmJSExportField<classtype,fieldtype,(bitpos/8),bytesize> jsexport_##fieldname(jsname, #fieldname, typeid(classtype *).name(), typeid(fieldtype *).name());
+
+#define asmjs_register_variable(jsname, type_jsname, address) \
+  AsmJSExportVar jsexport_##address(jsname, type_jsname, address);
