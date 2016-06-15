4841a2bd740832d63c5f31ae61e160ecc0ff482f
Pip <pipcet@gmail.com>
2016-06-15 01:38:04 +0000

    get first function call to work

--- wasm-experimentation/wasm-translate.scm
+++ wasm-experimentation/wasm-translate.scm
@@ -31,9 +31,12 @@
    ((number? e)
     (list 'i32.const e))
    ((eq? e '$rv)
-    (xlat-expr (list 'mem '(i32.const 4096))))
+    (xlat-expr (list 'mem '(i32.const 4096)) f-labels))
    ((not (list? e))
     (list 'get_local e))
+   ((and (eq? (car e) 'br) (eq? (cadr e) '$mainloop))
+;;    `(br ,(cadr f-labels))) XXX
+    `(br ,(car f-labels)))
    ((eq? (car e) 'return)
     (cons 'return (map (lambda (x) (xlat-expr x f-labels)) (cdr e))))
    ((eq? (car e) 'jump)
@@ -78,18 +81,8 @@
                                  (append (reverse block-labels) (list switch-default-label
                                                                       (list 'get_local '$dpc))))
                            (list while-in-label while-out-label)))
-          
           (list 'return '(i32.const 0)))
           ))
-
-(define (write-module f)
-  (let* ((ucnt 0)
-         (uniq (lambda ()
-                 (set! ucnt (1+ ucnt))
-                 (number->string ucnt))))
-    (write (list 'module
-                 (list 'import '$cp "console" "print" '(param i32))
-                 (list 'func "x" '$x '(param $dpc i32) '(param $sp i32) '(param $r0 i32) '(param $r1 i32) '(param $pc0 i32) '(param $rpc i32) (xlat-function f uniq))))))
 
 (define (emblocken rev-f labels s f-labels)
   (if (null? labels)
@@ -123,7 +116,7 @@
 
 (define (split-blocks-2 f)
   (let* ((r (split-blocks-r (reverse f))))
-    (if (null? (car r)) (cadr r)
+    (if (null? (cadr r)) (car r)
         (cons (cadr r) (car r)))))
 
 (define (split-blocks f)
@@ -145,24 +138,29 @@
                               (call_import $cp (i32.const 45))
                               (return)))))
 
-(define (make-module fdef)
-  (list 'module
-        (list 'memory 32 32)
-        (list 'import '$cp "console" "print" '(param i32))
-        `(func $peek (param $addr i32) (i32.load (get_local $addr)))
-        fdef
-        (list 'export "memory" 'memory)
-        ))
+(if #f (write (split-blocks '((call_import $cp (i32.const 42))
+                              (call_import $cp (i32.const 43))
+                              (call_import $cp (i32.const 44))
+                              (call_import $cp (i32.const 45))
+                              (return)))))
+
+(define (make-module fdefs)
+  `(module
+    (memory 32 32)
+    (import $cp "console" "print" (param i32))
+    (func $peek (param $addr i32) (i32.load (get_local $addr)))
+    ,@fdefs
+    (export "memory" memory)))
 
 (write (let* ((ucnt 0)
               (uniq (lambda ()
                       (set! ucnt (1+ ucnt))
                       (number->string ucnt))))
-         (car (map make-module (map (lambda (fdef)
-                (let* ((str (string-delete (car fdef) #\ 0))
-                       (sym (string->symbol (string-append "$" str))))
-                  (list 'func str sym '(param $dpc i32) '(param $sp1 i32) '(param $r0 i32) '(param $r1 i32) '(param $rpc i32) '(param $pc0 i32) '(result i32) '(local $sp i32) '(local $fp i32) '(local $r2 i32) '(local $r3 i32) '(local $r4 i32) '(local $r5 i32) '(local $r6 i32) '(local $r7 i32) '(local $i0 i32) '(local $i1 i32) '(local $i2 i32) '(local $i3 i32) '(local $i4 i32) '(local $i5 i32) '(local $i6 i32) '(local $i7 i32) '(local $f0 f64) '(local $f1 f64) '(local $f2 f64) '(local $f3 f64) '(local $f4 f64) '(local $f5 f64) '(local $f6 f64) '(local $f7 f64) (xlat-function (split-blocks (cdr fdef)) uniq))))
-              (eval (read) (interaction-environment)))))))
+         (make-module (map (lambda (fdef)
+                            (let* ((str (string-delete (car fdef) #\ 0))
+                                   (sym (string->symbol (string-append "$" str))))
+                              (list 'func str sym '(param $dpc i32) '(param $sp1 i32) '(param $r0 i32) '(param $r1 i32) '(param $rpc i32) '(param $pc0 i32) '(result i32) '(local $sp i32) '(local $fp i32) '(local $r2 i32) '(local $r3 i32) '(local $r4 i32) '(local $r5 i32) '(local $r6 i32) '(local $r7 i32) '(local $i0 i32) '(local $i1 i32) '(local $i2 i32) '(local $i3 i32) '(local $i4 i32) '(local $i5 i32) '(local $i6 i32) '(local $i7 i32) '(local $f0 f64) '(local $f1 f64) '(local $f2 f64) '(local $f3 f64) '(local $f4 f64) '(local $f5 f64) '(local $f6 f64) '(local $f7 f64) '(local $rp i32) (xlat-function (split-blocks (cdr fdef)) uniq))))
+              (eval (read) (interaction-environment))))))
 
 ;; (define (split-blocks f)
 ;;   (let ((blocks ())
