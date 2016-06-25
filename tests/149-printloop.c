753f5c14a89ce98b232e6c18398247c184d2e31c
Pip <pipcet@gmail.com>
2016-06-25 14:27:33 +0000

    tweaks

--- tests/149-printloop.c
+++ tests/149-printloop.c
@@ -3,6 +3,6 @@ void _start(void)
   volatile char x = 0;
 
   while (++x)
-    asm volatile("%0\n\t%0\n\t%0\n\t%0\n\tcall_import[4] __wasm_import_extcall"
+    asm volatile("%0\n\t%0\n\t%0\n\t%0\n\tcall_import[4] $extcall"
                  : : "r" (0xdeadbeef));
 }
