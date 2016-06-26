5227e3c2f7176e86749d74b337da5c35ac11a8b7
Pip <pipcet@gmail.com>
2016-06-26 16:41:36 +0000

    minor tweaks

--- runthis2.sh
+++ runthis2.sh
@@ -1,6 +1,6 @@
 ./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-gcc ./tests/004-hello-world-infloop-count.c
-./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-ld -T wasm-experimentation/wasm.lds a.out -o d.out
-./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-ld -T wasm-experimentation/wasm2.lds d.out -o b.out
+./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-ld -T wasm-experimentation/wasm.lds --no-check-sections a.out -o d.out
+./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-ld -T wasm-experimentation/wasm2.lds --no-check-sections d.out -o b.out
 ./wasmrewrite2 b.out > c.out
 cp c.out wasm-experimentation/wasm.wasm
 wasm64-virtual-wasm64-objcopy --dump-section .data=wasm-experimentation/wasm.data a.out
