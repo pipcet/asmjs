dc0fe0c63841b0dc0f4ae8b588a6d6032eab2a28
Pip <pipcet@gmail.com>
2016-06-27 14:02:28 +0000

    new test script

---
+++ runthis4.sh
@@ -0,0 +1,8 @@
+./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-gcc "$1"
+./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-ld -T wasm-experimentation/wasm.lds --no-check-sections a.out -o d.out
+./wasm64-virtual-wasm64/bin/wasm64-virtual-wasm64-ld -T wasm-experimentation/wasm2.lds --no-check-sections d.out -o b.out
+# ./wasmrewrite2 b.out > c.out
+./wasmrewrite3 b.out > c.out
+cp c.out wasm-experimentation/wasm.wasm
+wasm64-virtual-wasm64-objcopy --dump-section .data=wasm-experimentation/wasm.data a.out
+(cd wasm-experimentation/; JIT_OPTION_wasmTestMode=true ../common/bin/js ./wasm64lib.js)
