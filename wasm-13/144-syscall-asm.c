fd699a9bbf3f88dd92bccab23db9ec11e28f362a
Pip Cet <pipcet@gmail.com>
2016-11-25 23:57:00 +0000

    minor tweaks, update example, subrepos

--- wasm-13/144-syscall-asm.c
+++ wasm-13/144-syscall-asm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 extern int __thinthin_write (int, void *, int) __attribute__((stackcall));
 
@@ -7,4 +8,6 @@ int main(void)
   int i;
   for (i = 0; i < 5; i++)
     __thinthin_write(1, "hi there\n", 9);
+  for (i = 0; i < 5; i++)
+    write(1, "bye there\n", 10);
 }
