b4448a317dc41c6fef89d4e6bbc875813bc39be7
Pip Cet <pipcet@gmail.com>
2016-12-17 01:55:01 +0000

    make libdl a shared library in an example

---
+++ tests/173-libdl/005-libdl.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+asm(".include \"wasm32-import-macros.s\"");
+
+asm(".import3_pic thinthin,dlload,__thinthin_dlload");
+asm(".import3_pic thinthin,dlreaddep,__thinthin_dlreaddep");
+asm(".import3_pic thinthin,dlinstantiate,__thinthin_dlinstantiate");
+asm(".import3_pic thinthin,dlsym,__thinthin_dlsym");
+
+extern long __thinthin_dlload(const char *path, unsigned long *memp)
+  __attribute__((stackcall));
+extern long __thinthin_dlreaddep(long modi, char *mem, unsigned long len)
+  __attribute__((stackcall));
+extern long __thinthin_dlinstantiate(long modi, void *mem)
+  __attribute__((stackcall));
+extern void *__thinthin_dlsym(long modi, const char *symbol)
+  __attribute__((stackcall));
+
+void *dlopen(const char *path, int flags)
+{
+  long modi;
+  unsigned long mem;
+  char *buf;
+  long off;
+  long len;
+  char *p;
+
+  printf("dlopen %s\n", path);
+
+  if (!(flags & RTLD_LAZY) &&
+      !(flags & RTLD_NOW))
+    return NULL;
+
+  if (path)
+    {
+      modi = __thinthin_dlload(path, &mem);
+
+      off = 0;
+      len = 1;
+      buf = malloc(len);
+      buf[0] = 0;
+
+      while ((off += __thinthin_dlreaddep(modi, buf + off, len - off)) == len)
+        {
+          len *= 2;
+          buf = realloc(buf, len);
+        }
+
+      __thinthin_dlinstantiate(modi, mem ? malloc(mem) : NULL);
+
+      for (p = buf; p < buf+off; p += strlen(p) + 1)
+        dlopen(p, flags);
+    }
+  else
+    {
+      modi = 1;
+    }
+
+  return (void *)modi;
+}
+
+void *dlsym(void *handle, const char *symbol)
+{
+  return __thinthin_dlsym((long)handle, symbol);
+}
+
+int dlclose(void *handle)
+{
+  return 0;
+}
