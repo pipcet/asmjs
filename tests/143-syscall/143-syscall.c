8462ac12e39500bf423c3500433cc32c870facac
Pip Cet <pipcet@gmail.com>
2020-06-14 21:05:06 +0000

    fix tests

--- tests/143-syscall/143-syscall.c
+++ tests/143-syscall/143-syscall.c
@@ -1,9 +1,11 @@
-asm(".include \"143-import-macros.s\"");
-asm(".import3 thinthin,write,__thinthin_write");
+asm(".include \"wasm32-import-macros.s\"");
+asm(".import3_pic thinthin,write,__thinthin_write");
 
 extern __attribute__((stackcall)) int __thinthin_write(int, const char *, int);
 
-int main(void)
+void _start(void)
 {
   return __thinthin_write(1, "hi there\n", __builtin_strlen("hi there\n"));
 }
+
+int main(void) {}
--- tests/149-printloop/149-printloop.c
+++ tests/149-printloop/149-printloop.c
@@ -3,6 +3,6 @@ void _start(void)
   volatile char x = 0;
 
   while (++x)
-    asm volatile("%0\n\t%0\n\t%0\n\t%0\n\tcall_import[4] $extcall"
+    asm volatile("%0\n\t%0\n\t%0\n\t%0\n\tcall_indirect 0 $extcall"
                  : : "r" (0xdeadbeef));
 }
